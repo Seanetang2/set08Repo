d46b980b11504cb030bb71f1a6e1f982aed67b72
Peters Wisdom <peterswisdom603@gmail.com>
2023-11-10 15:41:56 +0100

    process

--- index.ts
+++ index.ts
@@ -7,3 +7,15 @@ const port: number = 3400;
 const Server = app.listen(port, () => {
   console.log("Server is up and running", port);
 });
+
+process.on("uncaughtException", (err: Error) => {
+  console.log("uncaughtException", err);
+  process.exit(1);
+});
+
+process.on("unhandleRejectionHandle", (reason: any) => {
+  console.log("unhandleRejectionHandle", reason);
+  Server.close(() => {
+    process.exit(1);
+  });
+});
